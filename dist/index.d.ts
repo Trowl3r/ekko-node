e3d39a01588fe2f3927804301422f478f8b5e177
Torben Bücker <Riaslike@gmail.com>
2021-05-31 23:51:25 +0200

    changed export database to db

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@
 declare const server: {
     createServer: (middlewareString?: string[]) => import("express-serve-static-core").Express;
 };
-declare const database: {
+declare const db: {
     connectDB: (mongoURI: string, options?: import("mongoose").ConnectOptions) => Promise<void>;
 };
-export { server, database };
+export { server, db };
--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.database = exports.server = void 0;
+exports.db = exports.server = void 0;
 var server_1 = require("./server/server");
 var database_1 = require("./database/database");
 var server = {
     createServer: server_1.createServer
 };
 exports.server = server;
-var database = {
+var db = {
     connectDB: database_1.connectDB
 };
-exports.database = database;
+exports.db = db;
